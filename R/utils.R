b4f9757c3617d367bda54fe961d12e2993291887
mnaegelin <71066917+mnaegelin@users.noreply.github.com>
2025-11-19 17:36:24 +0100

    read qwa data

--- R/utils.R
+++ R/utils.R
@@ -2,16 +2,24 @@
 #' @export
 read_QWAdata <- function(file_path, dataset_name = NULL, file_rings = NULL, file_cells = NULL) {
 
-  # create filepaths from path and dataset name
+  files <- list.files(file_path, full.names = TRUE)
+  cell_file <- stringr::str_subset(files, ".csv")
+  cell_file <- stringr::str_subset(cell_file, "cells")
   if (!is.null(dataset_name)) {
-    file_rings <- file.path(file_path, glue::glue("{dataset_name}_rings.csv"))
-    if (!file.exists(file_rings)) {
-      stop("Rings file not found under: ", file_rings)
-    }
-    file_cells <- file.path(file_path, glue::glue("{dataset_name}_cells.csv"))
-    if (!file.exists(file_rings)) {
-      stop("Cells file not found under: ", file_rings)
-    }
+    cell_file <- stringr::str_subset(cell_file, dataset_name)
+  }
+  ring_file <- stringr::str_subset(files, ".csv")
+  ring_file <- stringr::str_subset(ring_file, "rings")
+  if (!is.null(dataset_name)) {
+    ring_file <- stringr::str_subset(ring_file, dataset_name)
+  }
+
+  # create filepaths from path and dataset name
+  if (length(cell_file) != 1) {
+    stop("Cell file could not be (uniquely) identifierd under given path.")
+  }
+  if (length(ring_file) != 1) {
+    stop("Ring file could not be (uniquely) identifierd under given path.")
   }
 
   # TODO: implement for providing file names directly
@@ -20,18 +28,17 @@ read_QWAdata <- function(file_path, dataset_name = NULL, file_rings = NULL, file
 
   # TODO: fix with final columns
   QWA_data$rings <- vroom::vroom(
-    file_rings,
+    ring_file,
     col_types = c(.default = "d",
-                  tree_code = "c", woodpiece_code = "c", slide_code = "c", image_code = "c",
-                  year = "i", cno = "i", innermost_ring = "l", outermost_ring = "l",
-                  incomplete_inner= "l", incomplete_innerv2= "l", incomplete_ring= "l", incomplete_fct_check = "l",
-                  missing_ring= "l", no_MRW_other= "l", missing_ringV2= "l", duplicate_ring= "l", duplicate_rank= "i"))
+                  tree_label = "c", woodpiece_label = "c", slide_label = "c", image_label = "c",
+                  year = "i", cno = "i", incomplete_ring = "l", missing_ring = "l",
+                  duplicate_ring= "l", exclude_dupl= "l", exclude_issues= "l"))
 
   # TODO: final cols, EW_LW as logical?
   QWA_data$cells <- vroom::vroom(
-    file_cells,
+    cell_file,
     col_types = c(.default = "d",
-                  image_code = "c", year = "i", xpix = "i", ypix = "i", nbrno = "i", nbrid = "i", sector100 = "i", ew_lw = "c")
+                  image_label = "c", year = "i", xpix = "i", ypix = "i", nbrno = "i", nbrid = "i", sector100 = "i", ew_lw = "c")
     )
   # TODO: check for problems?
 
@@ -40,3 +47,31 @@ read_QWAdata <- function(file_path, dataset_name = NULL, file_rings = NULL, file
 
   return(QWA_data)
 }
+
+
+read_QWA_metadata <- function(file_path, dataset_name = NULL, force_valid = TRUE) {
+
+  files <- list.files(file_path, full.names = TRUE)
+  meta_file <- stringr::str_subset(files, ".json")
+  meta_file <- stringr::str_subset(meta_file, "metadata")
+  if (!is.null(dataset_name)) {
+    meta_file <- stringr::str_subset(meta_file, dataset_name)
+  }
+
+  if (length(meta_file) != 1) {
+    stop("Metadata file could not be (uniquely) identifierd under given path.")
+  }
+
+  metadata <- jsonlite::read_json(meta_file, simplifyVector = TRUE)
+
+  meta_schema <- jsonvalidate::json_schema$new(
+    #"../rxs2tria/inst/extdata/json_schema/20251007_tria_shinyext_schema.json",
+    system.file("extdata", "json_schema/20251007_tria_shinyext_schema.json", package = "rxs2tria"),
+    engine = "ajv"
+  )
+  validation <- meta_schema$validate(meta_file, error = force_valid)
+  message("Schema validation check passed? ", validation)
+
+  metadata
+}
+
---
+++ example_workflow.R
@@ -0,0 +1,97 @@
+################################################################################
+# installation
+library(devtools)
+load_all()
+
+#remotes::install_github('mnaegelin/rxs2tria')
+#library(rxs2tria)
+
+################################################################################
+# set path to the input and output data
+# where the ROXAS files are (can contain subfolders)
+path_in <- '../example_data/rxs2tria_in/QWA_Arzac2024'
+
+# where output files should be saved to
+path_out <- '../example_data/rxs2tria_out'
+
+dataset_name <- 'POGSTO2024' # used to name the resulting output files
+
+
+################################################################################
+# get overview of data to be read and extract data structure from filenames
+files <- get_roxas_files(path_in)
+
+# example1: `{site}_{species}_{tree}{woodpiece}_{slide}_{image}` (with 2digit tree identifier and optional woodpiece)
+pattern <- "(?<site>[:alnum:]+)_(?<species>[:alnum:]+)_(?<tree>[:alnum:][:alnum:])(?<woodpiece>[:alnum:]*)_(?<slide>[:alnum:]+)_(?<image>[:alnum:]+)"
+
+# example2: `{site}_{species}_{tree}_{slide}_{image}`
+#pattern <- "(?<site>[:alnum:]+)_(?<species>[:alnum:]+)_(?<tree>[:alnum:].+)_(?<slide>[:alnum:]+)_(?<image>[:alnum:]+)"
+
+df_structure <- extract_data_structure(files, pattern)
+
+
+################################################################################
+# read available metadata
+df_rxsmeta <- collect_metadata_from_files(df_structure,
+                                          roxas_version='classic')
+
+################################################################################
+# complete the required metadata form via the Shiny app
+launch_metadata_app()
+# save the json metadata file from the app output once completed
+
+################################################################################
+# read raw cells/rings data
+QWA_data <- collect_raw_data(df_structure)
+
+
+################################################################################
+# remove outliers
+# NOTE: ROXAS does some threshold based outlier checks and assigns these a
+# a negative value. Here, we replace these outliers with NAs)
+QWA_data <- remove_outliers(QWA_data)
+
+
+################################################################################
+# complete measures (add EW/LW estimations)
+QWA_data <- complete_cell_measures(QWA_data)
+
+
+################################################################################
+# clean raw data
+QWA_data <- validate_QWA_data(QWA_data, df_rxsmeta)
+
+# this creates some initial flags based on the data only, namely missing, incomplete and duplicate flags
+
+
+################################################################################
+# save preprocessed data to files
+fname_out <- file.path(
+  path_out,
+  glue::glue("{format(Sys.Date(), '%Y%m%d')}_TRIA_{dataset_name}")
+)
+readr::write_csv(
+  QWA_data$cells,
+  paste0(fname_out, '_cells.csv.gz'))
+
+readr::write_csv(QWA_data$rings,
+                 paste0(fname_out, '_rings.csv'))
+
+
+
+
+################################################################################
+# provide user input on ring flags
+# interactively in shiny app (NOT READY YET)
+# TODO: adapt to new flag logic (need to calculate duplicate_rank in app)
+# launch_coverage_app()
+
+
+
+################################################################################
+# READ QWA data
+file_path <- "../example_data/rxs2tria_out"
+QWA_data <- read_QWAdata(file_path)
+
+file_path <- "../example_data/tria_download"
+QWA_metadata <- read_QWA_metadata(file_path)
